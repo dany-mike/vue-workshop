50cbf167fb81757214147955b828727b2a11b543
Dany-Mike <dmjoseph@kaliop.com>
2021-11-13 19:10:22 +0100

    workshop part one done

---
+++ front/src/components/atoms/a-postal-code.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="m-postal">
+    <h1>{{ postalCode }}</h1>
+    <!-- <ASelect
+      :cities="cities"
+    >
+    </ASelect> -->
+    <AButton
+      @click.native="handleBack"
+    >
+      Back
+    </AButton>
+  </div>
+</template>
+
+<script>
+import AButton from '@/components/atoms/a-button.vue'
+
+export default {
+  name: 'APostalCode',
+  components: {
+      AButton,
+  },
+  props: {
+      postalCode: {
+          type: String,
+          default: ''
+      },
+      cities: {
+        type: Array,
+        default: () => []
+      }
+  },
+  methods: {
+      handleBack () {
+          this.$emit('on-back')
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
