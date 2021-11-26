df2efe06e0683f42179858a40a7544fcdd2e521f
Dany-Mike <dmjoseph@kaliop.com>
2021-11-26 16:01:41 +0100

    initial commit vue workshop frontend

--- api/routes/cities.route.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const { getCityByPostalCode } = require('../controllers/city.controller.js');
-
-router.post('/', getCityByPostalCode);
-
-module.exports = router;
