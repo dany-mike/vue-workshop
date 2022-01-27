d2bf5c12b4a802b6fa069fa8364468e6a3ed48ec
Dany-Mike <dmjoseph@kaliop.com>
2022-01-27 11:53:38 +0100

    [vue-18n-setup] installation done

--- src/components/organisms/o-city.vue
+++ src/components/organisms/o-city.vue
@@ -66,7 +66,7 @@ export default {
       },
       formattedWeatherForecast: {
         type: Array,
-        default: () => {}
+        default: () => []
       },
   },
   methods: {
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from "./store";
 import VueCompositionAPI from '@vue/composition-api'
 import vSelect from 'vue-select'
+import VueI18n from 'vue-i18n'
 import 'vue-select/dist/vue-select.css';
 
 
@@ -12,6 +13,7 @@ import '@/assets/css/tailwind.css'
 Vue.config.productionTip = false
 Vue.component('v-select', vSelect)
 Vue.use(VueCompositionAPI)
+Vue.use(VueI18n)
 
 new Vue({
   router,
