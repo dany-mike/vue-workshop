8ec04e84a7a76bf9273fd95e0eccabb976d88f34
Dany-Mike <dmjoseph@kaliop.com>
2022-01-27 16:56:37 +0100

    [loader] install vue-loading-overlay@^3.0

--- src/main.js
+++ src/main.js
@@ -3,15 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from "./store";
 import i18n from './i18n'
+
 import VueCompositionAPI from '@vue/composition-api'
 import vSelect from 'vue-select'
-import 'vue-select/dist/vue-select.css';
+// import VueLoading from 'vue-loading-overlay';
 
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'vue-select/dist/vue-select.css';
 import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 Vue.component('v-select', vSelect)
 Vue.use(VueCompositionAPI)
+// Vue.use(VueLoading)
+
 
 new Vue({
   router,
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
+  <div class="about" ref="homeCont">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+// export default {
+//   mounted() {
+//   let homeCont = this.$refs.homeCont
+//   this.$loading.show(
+//     {
+//       container: homeCont,
+//       loader: this.loader,
+//     }
+//   );
+//   }
+// }
+</script>
