68b011dc79e0c06fd23fa8bf8783d8ac0de2cce4
Dany-Mike <dmjoseph@kaliop.com>
2022-01-28 16:31:50 +0100

    [vue-leaflet] add trad and create m-leaflet-map component

---
+++ src/components/molecules/m-leaflet-map.vue
@@ -0,0 +1,37 @@
+<template>
+  <l-map style="height: 500px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker 
+    :lat-lng="markerLatLng"
+    :icon="markerIcon"
+    ></l-marker>
+  </l-map>
+</template>
+
+<script>
+import L from 'leaflet'
+
+export default {
+  name: 'MLeafletMap',
+  data () {
+    const myMarkerIcon = L.icon({
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconSize: [24,36],
+      iconAnchor: [12,36]
+    })
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:'&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159],
+      markerIcon: myMarkerIcon
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,38 +1,15 @@
 <template>
-  <div class="about" ref="aboutContainer">
-    <h1>This is a map page</h1>
-    <l-map style="height: 500px" :zoom="zoom" :center="center">
-      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-marker 
-      :lat-lng="markerLatLng"
-      :icon="markerIcon"
-      ></l-marker>
-    </l-map>
+  <div class="about px-6" ref="aboutContainer">
+    <p class="font-bold text-lg my-4">{{ $t('Click on the map to get the weather') }}</p>
+    <MLeafletMap />
   </div>
 </template>
 
 <script>
-import L from 'leaflet'
-
+import MLeafletMap from '@/components/molecules/m-leaflet-map'
 export default {
-  data () {
-
-    const myMarkerIcon = L.icon({
-      iconUrl: require('leaflet/dist/images/marker-icon.png'),
-      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-      iconSize: [24,36],
-      iconAnchor: [12,36]
-    })
-
-    return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution:'&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159],
-      markerIcon: myMarkerIcon
-    };
+  components:{
+    MLeafletMap
   }
 }
 </script>
