a1ad2f5156bf08328c2eb89ef2111a78e9087b3a
Dany-Mike <dmjoseph@kaliop.com>
2022-01-28 15:36:10 +0100

    [vue-leaflet] vue-leaflet map implemented in map page

--- src/main.js
+++ src/main.js
@@ -6,14 +6,19 @@ import i18n from './i18n'
 
 import VueCompositionAPI from '@vue/composition-api'
 import vSelect from 'vue-select'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import VueLoading from 'vue-loading-overlay';
 
+import 'leaflet/dist/leaflet.css';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import 'vue-select/dist/vue-select.css';
 import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 Vue.component('v-select', vSelect)
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 Vue.use(VueCompositionAPI)
 Vue.use(VueLoading)
 
--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about" ref="aboutContainer">
     <h1>This is a map page</h1>
+    <l-map style="height: 300px" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="markerLatLng"></l-marker>
+    </l-map>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159]
+    };
+  }
+}
+</script>
