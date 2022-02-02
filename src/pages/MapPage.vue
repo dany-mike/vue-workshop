1469accf43cf344ac54f4376b249ed6cf4c06dd2
Dany-Mike <dmjoseph@kaliop.com>
2022-02-02 15:28:47 +0100

    [vue-leaflet] getWeather when I click on the map and handle display

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="about px-6" ref="aboutContainer">
     <p class="font-bold text-lg my-4">{{ $t('Click on the map to get the weather') }}</p>
-    <MLeafletMap />
+    <MLeafletMap @m-leaflet-map::on-fetch-weather-by-coord="setWeatherData" />
+    <MCurrentWeather 
+    v-if="isCurrentWeather"
+    :current-weather="currentWeather" 
+    />
   </div>
 </template>
 
 <script>
 import MLeafletMap from '@/components/molecules/m-leaflet-map'
+import MCurrentWeather from '@/components/molecules/m-current-weather.vue'
 export default {
   components:{
-    MLeafletMap
+    MLeafletMap,
+    MCurrentWeather
+  },
+  data () {
+    return {
+      currentWeather: {},
+    }
+  },
+  methods: {
+    setWeatherData(currentWeather) {
+      this.currentWeather = currentWeather
+    }
+  },
+  computed: {
+    isCurrentWeather() {
+      return Object.keys(this.currentWeather).length !== 0
+    }
   }
 }
 </script>
